f86bc62be0bbe67b6f0d3399b4a3c980fb385717
Keith Smiley <keithbsmiley@gmail.com>
2016-03-23 15:36:58 -0700

    Fix code coverage with multiple directories
    This fixes an issue where there are multiple matches of `*.framework`
    which breaks the command using this variable

--- Resources/coverage.sh
+++ Resources/coverage.sh
@@ -4,5 +4,5 @@ set -e
 
 profdata="$1"
 coverage_dir="$2"
-framework=$(find "$coverage_dir" -name "*.framework")
+framework=$(find "$coverage_dir" -name "*.framework" | head -1)
 xcrun llvm-cov show -instr-profile "$profdata" "$framework/Mapper" > "coverage.txt"
