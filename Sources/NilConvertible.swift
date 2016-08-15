b3835b8cafeb0f9d1b9729ac860e535198f0f35e
Blair McArthur <blair.mcarthur@skipthedishes.com>
2016-08-15 12:39:50 -0500

    Created a NilConvertible protocol which accepts optional AnyObject values to convert, unlike Convertible which only allows non-optional AnyObject values.

---
+++ Sources/NilConvertible.swift
@@ -0,0 +1,20 @@
+public protocol NilConvertible {
+    /// This typealias allows us to enforce the returned value is of type Self, without requiring
+    /// implementations to return a value using `self.init`
+    associatedtype ConvertedType = Self
+    
+    /**
+     `fromMap` returns the expected value based off the provided input, this allows you to attempt
+     to cast the value to anything you'd like, and perform any manipulation on it (don't use this as a
+     conversion mechanism, instead see Transform)
+     
+     - parameter value: Any value (probably from the data source's value for the given field) to create
+     the expected object with
+     
+     - throws: Any error from your custom implementation, MapperError.ConvertibleError is recommended
+     
+     - returns: The successfully created value from the given input
+     */
+    @warn_unused_result
+    static func fromMap(value: AnyObject?) throws -> ConvertedType
+}
