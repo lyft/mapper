5410c260068965d0dc3f2561064295800050d7a4
Daniel Duan <daniel@duan.ca>
2017-09-19 11:01:24 -0700

    Support U?Int(32|64) Conversion Natively (#117)
    These are built-in types that applications use often to eliminate variants in
    32bit/64bit platforms. A Unix timestamp in milliseconds can't be parsed from
    json to an `Int` on 32bit devices, for example.

--- Sources/DefaultConvertible.swift
+++ Sources/DefaultConvertible.swift
@@ -30,7 +30,11 @@ extension NSArray: DefaultConvertible {}
 
 extension String: DefaultConvertible {}
 extension Int: DefaultConvertible {}
+extension Int32: DefaultConvertible {}
+extension Int64: DefaultConvertible {}
 extension UInt: DefaultConvertible {}
+extension UInt32: DefaultConvertible {}
+extension UInt64: DefaultConvertible {}
 extension Float: DefaultConvertible {}
 extension Double: DefaultConvertible {}
 extension Bool: DefaultConvertible {}
