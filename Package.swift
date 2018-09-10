e6a420a9b923a552933ac703cdb11558ebe9043e
Keith Smiley <keithbsmiley@gmail.com>
2018-09-09 23:19:16 -0700

    Update for Swift 4.2
    There aren't any required syntax changes here, but I have updated the
    Package.swift for breaking SwiftPM changes. Without this change you
    cannot compile Mapper in Swift 4.2 mode if you depend on it with SwiftPM

--- Package.swift
+++ Package.swift
@@ -1,3 +1,14 @@
+// swift-tools-version:4.2
 import PackageDescription
 
-let package = Package(name: "Mapper")
+let package = Package(
+    name: "Mapper",
+    products: [
+        .library(name: "Mapper", targets: ["Mapper"]),
+    ],
+    targets: [
+        .target(name: "Mapper", path: "Sources"),
+        .testTarget(name: "MapperTests", dependencies: ["Mapper"]),
+    ],
+    swiftLanguageVersions: [.v4, .v4_2]
+)
